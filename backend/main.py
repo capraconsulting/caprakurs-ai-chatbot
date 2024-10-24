76be23158ae8a85e554ad53841727a590509150d
aashiltf <aas-ten@hotmail.com>
2024-10-24 16:06:16 +0200

    Add mistral-7b-instruct-v02 example

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,29 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+
+
+messages = [
+    {"role": "user", "content": "What is your favourite condiment?"},
+    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+]
+
+encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+
+model_inputs = encodeds.to()
+model.to()
+
+generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+decoded = tokenizer.batch_decode(generated_ids)
+print(decoded[0])
+
+
+
+
 app = FastAPI()
 
 class Query(BaseModel):
@@ -15,3 +38,5 @@ def read_root():
 @app.post("/prompt")
 def post_prompt(query: Query):
     return {"answer": "Hello world"}
+
+
