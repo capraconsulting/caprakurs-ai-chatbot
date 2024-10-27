9b260cd03197ad3eb74112eacafd79f2d80855e5
Vemund Santi <vemund.santi@ruter.no>
2024-10-27 20:25:13 +0100

    Simplify React App. Use only TailwindCSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,48 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import "./App.css";
-import clsx from "clsx";
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 type ChatMessage = {
   message: string;
-  role: "user" | "assistant" | "system";
+  role: 'user' | 'assistant' | 'system';
 };
-const aiWorker = new Worker("worker.js", { type: "module" });
+const aiWorker = new Worker('worker.js', { type: 'module' });
 
-function App() {
-  const modelName = "Xenova/Qwen1.5-0.5B-Chat";
-  // Alternativ 1: Xenova/Qwen1.5-0.5B-Chat
-  // Alternativ 2: Felladrin/onnx-TinyMistral-248M-Chat-v2
-  // Alternativ 3: Felladrin/onnx-Pythia-31M-Chat-v1
+const modelName = 'Xenova/Qwen1.5-0.5B-Chat';
+// Alternativ 1: Xenova/Qwen1.5-0.5B-Chat
+// Alternativ 2: Felladrin/onnx-TinyMistral-248M-Chat-v2
+// Alternativ 3: Felladrin/onnx-Pythia-31M-Chat-v1
 
-  const sendButton = useRef<HTMLButtonElement>(null);
-  const chatInput = useRef<HTMLInputElement>(null);
+export default function App() {
   const chatMessages = useRef<HTMLDivElement>(null);
-  const [loadResponse, setLoadResponse] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+  const [modelStatus, setModelStatus] = useState<'initial' | 'loading' | 'ready'>('initial');
+  const [chatStatus, setChatStatus] = useState<'loading' | 'ready'>('ready');
+
+  const [message, setMessage] = useState('');
   const [messages, setMessages] = useState<ChatMessage[]>([]);
 
-  const addMessage = useCallback(
-    (message: string, role: ChatMessage["role"]) => {
+  const appendMessage = useCallback(
+    (message: string, role: ChatMessage['role']) => {
       setMessages((messages) => [...messages, { message, role }]);
     },
     [setMessages]
   );
 
-  const chat = (message: string) => {
-    setLoadResponse(true);
+  const downloadModel = useCallback(() => {
+    setModelStatus('loading');
     aiWorker.postMessage({
-      action: "chat",
+      action: 'download',
+      modelURL: modelName,
+    });
+  }, [setModelStatus]);
+
+  const sendMessage = useCallback(() => {
+    setChatStatus('loading');
+    appendMessage(message, 'user');
+    aiWorker.postMessage({
+      action: 'chat',
       content: message,
     });
-  };
-
-  const download = (modelURL: string) => {
-    setLoading(true);
-
-    aiWorker.postMessage({
-      action: "download",
-      modelURL: modelURL,
-    });
-  };
+    setMessage('');
+  }, [message, appendMessage, setMessage]);
 
   useEffect(() => {
     if (chatMessages.current) {
@@ -55,94 +53,97 @@ function App() {
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       const aiResponse = event.data;
-      if (aiResponse.status === "ready") {
-        addMessage(
-          `Model ready! More information here: ${aiResponse.modelURL}`,
-          "system"
-        );
-        setLoading(false);
+      if (aiResponse.status === 'ready') {
+        setModelStatus('ready');
       } else if (aiResponse.result) {
-        addMessage(aiResponse.result, "assistant");
-        setLoadResponse(false);
+        appendMessage(aiResponse.result, 'assistant');
+        setChatStatus('ready');
       }
     };
-
-    aiWorker.addEventListener("message", handleMessage);
-
+    aiWorker.addEventListener('message', handleMessage);
     return () => {
-      aiWorker.removeEventListener("message", handleMessage);
+      aiWorker.removeEventListener('message', handleMessage);
     };
-  }, [addMessage]);
-
-  const sendMessage = () => {
-    const question = message;
-
-    addMessage(question, "user");
-    chat(question);
-    setMessage("");
-  };
+  }, [appendMessage]);
 
   return (
-    <div id="container">
-      <div
-        id="chat-container"
-        className="h-full w-full flex flex-row items-center"
-      >
-        <div id="chat-header">
+    <div className="flex w-screen h-screen justify-center items-center bg-[#333]">
+      <div className="flex w-[1000px] max-w-full h-[80%] flex-col bg-white p-4 border-3 border-gray-600 overflow-y-scroll">
+        <div className="flex flex-row justify-center">
           <h2 className="text-2xl">AWS AI Workshop: {modelName}</h2>
         </div>
-        <div>
-          <button
-            className="h-10 flex justify-center items-center"
-            onClick={() => download(modelName)}
-          >
-            Load {modelName}
-          </button>
-        </div>
-        <div
-          ref={chatMessages}
-          className="w-full flex flex-col items-center justify-center"
-        >
-          {loading && <div className="spinner"></div>}
-          {messages.map((message, index) => (
-            <div className={clsx(message.role, "chat-message")} key={index}>
-              {message.message}
-            </div>
-          ))}
-        </div>
-        {loadResponse && (
-          <div className="flex flex-col items-center">
-            Waiting for LLM response...
+        {modelStatus === 'initial' && (
+          <div className="flex flex-col justify-center items-center grow">
+            <button className="text-white bg-[#ff5c00] text-xl px-4 py-2" onClick={downloadModel}>
+              Download model
+            </button>
+          </div>
+        )}
+        {modelStatus === 'loading' && (
+          <div className="flex flex-col justify-center items-center grow">
             <div className="spinner"></div>
           </div>
         )}
-        <div id="chat-input-container">
-          {!loading && (
-            <div className="w-full p-10 flex flex-row justify-center">
+        {modelStatus === 'ready' && (
+          <>
+            {messages.length === 0 && (
+              <div className="flex flex-col justify-center items-center text-[#666] font-mono text-center grow">
+                Start chatting with {modelName}!
+              </div>
+            )}
+            {messages.length > 0 && (
+              <div
+                ref={chatMessages}
+                className="w-full flex flex-col items-center justify-center h-[50vh] overflow-y-auto grow"
+              >
+                {messages.map((message, index) => (
+                  <div
+                    className={`chat-message ${
+                      message.role === 'user'
+                        ? 'bg-[#faebd7] self-end'
+                        : message.role === 'assistant'
+                        ? 'bg-[#f9cd93] self-start'
+                        : ''
+                    } w-[80%] p-4 mb-4 break-words`}
+                    key={index}
+                  >
+                    {message.message}
+                  </div>
+                ))}
+              </div>
+            )}
+            <div className="flex w-full p-10 flex-row justify-center">
               <input
-                className="w-full"
+                className="w-full p-4 text-base border border-[#ff5c00] mr-4"
                 type="text"
-                ref={chatInput}
                 value={message}
+                disabled={chatStatus === 'loading'}
                 onChange={(event) => {
                   setMessage(event.target.value);
                 }}
                 onKeyDown={(event) => {
-                  if (event.key === "Enter") {
+                  if (event.key === 'Enter') {
                     sendMessage();
                   }
                 }}
                 placeholder="Type your message..."
               />
-              <button ref={sendButton} onClick={sendMessage} disabled={loading}>
-                Send
-              </button>
+              <div className="flex flex-col items-center justify-center h-full w-16">
+                {chatStatus === 'loading' ? (
+                  <div className="spinner"></div>
+                ) : (
+                  <button
+                    onClick={sendMessage}
+                    className="text-white text-base border border-[#ff5c00] h-full w-full bg-[#ff5c00] cursor-pointer"
+                  >
+                    Send
+                  </button>
+                )}
+              </div>
             </div>
-          )}
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
 }
-
-export default App;
